680978aec498ea8564c205ba846b4166bc71937d
hans.yonathan@falinwa.com <hans.yonathan@falinwa.com>
2014-11-14 11:54:49 +0700

    commit

--- fal_purchase_cp/purchase.py
+++ fal_purchase_cp/purchase.py
@@ -5,7 +5,6 @@ import openerp.addons.decimal_precision as dp
 import time
 
 class purchase_order(orm.Model):
-    _name = "purchase.order"
     _inherit = "purchase.order"
     
     _columns = {
@@ -16,12 +15,11 @@ class purchase_order(orm.Model):
     _defaults = {
         'fal_user_id': lambda obj, cr, uid, context: uid,
     }
-    
-    def onchange_partner_id(self, cr, uid, ids, partner_id):
-        res = super(purchase_order, self).onchange_partner_id(cr, uid, ids, partner_id)
-        partner = self.pool.get('res.partner').browse(cr, uid, partner_id)
+
+    def onchange_partner_id(self, cr, uid, ids, part, context=None):
+        res = super(purchase_order, self).onchange_partner_id(cr, uid, ids, part, context=context)
+        partner = self.pool.get('res.partner').browse(cr, uid, part, context=context)
         res['value']['fal_partner_contact_person_id'] = partner.child_ids and partner.child_ids[0].id or False
-        print res
         return res
         
 #end of purchase_order()

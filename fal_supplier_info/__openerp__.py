2e45a8c55e4f6d6b82430277e5a87189a43332de
hans.yonathan@falinwa.com <hans.yonathan@falinwa.com>
2015-01-05 09:28:32 +0700

    commit

---
+++ fal_supplier_info/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "GEN-34_Supplier Info Menu",
+    "version": "1.0",
+    'author': 'Falinwa Hans',
+    "description": """
+    Module to give supplier info menu.
+    """,
+    "depends" : ['base', 'product', 'sale', 'purchase'],
+    'init_xml': [],
+    'data': [
+    ],
+    'update_xml': [
+        'product_view.xml'
+    ],
+    'css': [],
+    'js' : [
+    ],
+    'qweb': [],
+    'installable': True,
+    'active': False,
+    'application' : False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

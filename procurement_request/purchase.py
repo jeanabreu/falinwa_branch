0a928d6cc7f3d4d83aca189b2f31b3e7e9a46ee4
hans.yonathan@falinwa.com <hans.yonathan@falinwa.com>
2015-01-21 15:16:21 +0700

    commit

--- procurement_request/purchase.py
+++ procurement_request/purchase.py
@@ -177,7 +177,7 @@ class purchase_order(orm.Model):
     def _prepare_order_line_move(self, cr, uid, order, order_line, picking_id, group_id, context=None):
         res = super(purchase_order, self)._prepare_order_line_move(cr, uid, order, order_line, picking_id, group_id, context=context)
         for rex in res:
-            res['fal_warehouse_manager_comment'] = order_line.warehouse_manager_comment
+            rex['fal_warehouse_manager_comment'] = order_line.warehouse_manager_comment
         return res
     
 #end of purchase_order()

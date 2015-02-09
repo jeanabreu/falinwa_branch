7422193d624c7e7f657a5097faff29b92216138e
hans.yonathan@falinwa.com <hans.yonathan@falinwa.com>
2015-02-09 15:23:24 +0700

    commit

--- fal_web_interactive_map/static/src/js/fal_interactive_map.js
+++ fal_web_interactive_map/static/src/js/fal_interactive_map.js
@@ -1,13 +1,24 @@
   function hideMarkDescription(id){
-    setTimeout(function(){
-        document.getElementById(id).style.display = "none";
-        },3500);
+    document.getElementById(id).style.display = "none";
   };
   
   function showMarkDescription(mark_id, des_id){
     var des_top = parseInt(document.getElementById(mark_id).offsetTop, 10) - 129 + 'px';
-    var des_left = parseInt(document.getElementById(mark_id).offsetLeft, 10) - 209 + 'px' ;
+    var des_left = parseInt(document.getElementById(mark_id).offsetLeft, 10) - 218 + 'px' ;
     document.getElementById(des_id).style.display = "block";
     document.getElementById(des_id).style.top = des_top;
     document.getElementById(des_id).style.left = des_left;
   };
+
+(function () {
+    'use strict';
+    var website = openerp.website;  
+    console.log(website.snippet);
+    console.log(website.snippet.options.slider);
+    website.snippet.options.slider.include({
+        start : function () {
+            this._super();
+            this.$target.carousel({interval: 1});
+        },    
+    }); 
+})();

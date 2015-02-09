694ae945da8f2473481cbe6377fc7e1aab8c5d5b
hans.yonathan@falinwa.com <hans.yonathan@falinwa.com>
2015-02-09 09:16:02 +0700

    commit

---
+++ fal_web_interactive_map/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "WEB-01_Interactive Map",
+    "version": "1.0",
+    'author': 'Falinwa Hans',
+    "description": """
+    Module to give Interactive Map Block.
+    """,
+    "depends" : ['base', 'website'],
+    'init_xml': [],
+    'data': [
+    ],
+    'update_xml': [
+        'views/snippets.xml',
+    ],
+    'css': [],
+    'js' : [
+    ],
+    'qweb': [],
+    'installable': True,
+    'active': False,
+    'application' : False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ fal_web_interactive_map/static/src/js/fal_interactive_map.js
@@ -0,0 +1,13 @@
+  function hideMarkDescription(id){
+    setTimeout(function(){
+        document.getElementById(id).style.display = "none";
+        },3500);
+  };
+  
+  function showMarkDescription(mark_id, des_id){
+    var des_top = parseInt(document.getElementById(mark_id).offsetTop, 10) - 129 + 'px';
+    var des_left = parseInt(document.getElementById(mark_id).offsetLeft, 10) - 209 + 'px' ;
+    document.getElementById(des_id).style.display = "block";
+    document.getElementById(des_id).style.top = des_top;
+    document.getElementById(des_id).style.left = des_left;
+  };

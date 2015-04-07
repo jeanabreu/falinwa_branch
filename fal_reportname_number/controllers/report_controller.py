d84f89a4726e5c40cf55b1c5e77d1fb5c11b8e61
hans.yonathan@falinwa.com <hans.yonathan@falinwa.com>
2015-04-07 10:27:05 +0700

    commit

--- fal_reportname_number/controllers/report_controller.py
+++ fal_reportname_number/controllers/report_controller.py
@@ -6,28 +6,57 @@ from openerp import http
 import simplejson
 
 class FalReportController(ReportController):
+
     @route(['/report/download'], type='http', auth="user")
     def report_download(self, data, token):
-        print 'masuk'
-        order_obj = http.request.env['sale.order']
+        
+        sale_obj = http.request.env['sale.order']
+        purchase_obj = http.request.env['purchase.order']
+        invoice_obj = http.request.env['account.invoice']
+        picking_obj = http.request.env['stock.picking']
+        mo_obj = http.request.env['mrp.production']
+        payslip_obj = http.request.env['hr.payslip']
+        expense_obj = http.request.env['hr.expense.expense']
+        
         requestcontent = simplejson.loads(data)
+
         url, type = requestcontent[0], requestcontent[1]
-# url = u'/report/pdf/sale.report_saleorder/37'
-# type = u'qweb-pdf'
-        assert type == 'qweb-pdf'
-        reportname = url.split('/report/pdf/')[1].split('?')[0]
-# reportname = u'sale.report_saleorder/37'
-        reportname, docids = reportname.split('/')
-# reportname = u'sale.report_saleorder'
-# docids = 37
-        assert docids
-        object = order_obj.browse(int(docids))
-
-        name = object.name
-# name = 2014-DE000020
-        filename = object.name
-# filename = PT_2014-DE000020
-
+        # url = u'/report/pdf/sale.report_saleorder/37'
+        # type = u'qweb-pdf'
+        
         response = ReportController().report_download(data, token)
-        response.headers.set('Content-Disposition', 'attachment; filename=%s.pdf;' % filename)
+        
+        
+        if type == 'qweb-pdf':
+            reportname = url.split('/report/pdf/')[1].split('?')[0]
+            # reportname = u'sale.report_saleorder/37'
+
+            reportname, docids = reportname.split('/')
+            # reportname = u'sale.report_saleorder'
+            # docids = 37
+
+            assert docids
+
+            if reportname in ['sale.report_saleorder', 'purchase.report_purchaseorder', 'account.report_invoice', 'stock.report_picking', 'mrp.report_mrporder', 'hr_payroll.report_payslip', 'hr_expense.report_expense']:
+                filename = reportname
+                if reportname == 'sale.report_saleorder':
+                    object = sale_obj.browse(int(docids))
+
+                    filename = object.name
+                    # filename = SO0012
+
+                if reportname == 'purchase.report_purchaseorder':
+                    object = purchase_obj.browse(int(docids))
+
+                    filename = object.name
+                    # filename = SO0012
+
+                if reportname == 'account.report_invoice':
+                    object = invoice_obj.browse(int(docids))
+                    if object.number:
+                        filename = object.number
+                    # filename = SO0012
+                
+                response.headers.set('Content-Disposition', 'attachment; filename=%s.pdf;' % filename)
+
         return response

2aae9473b0e6964bec4653bd9740e9fdad527161
hans.yonathan@falinwa.com <hans.yonathan@falinwa.com>
2015-04-09 17:09:40 +0700

    commit

--- ap_sourcing_ext/account.py
+++ ap_sourcing_ext/account.py
@@ -17,3 +17,18 @@ class account_invoice(models.Model):
         
 #end of account_invoice()
 
+class res_partner_bank(models.Model):
+    _name = "res.partner.bank"
+    _inherit = "res.partner.bank"
+    
+    #fields start here
+    fal_bank_street = fields.Char('Bank Street')
+    fal_bank_zip = fields.Char('Bank Zip', change_default=True, size=24)
+    fal_bank_city = fields.Char('Bank City')
+    fal_bank_country_id = fields.Many2one('res.country', 'Bank Country',
+       change_default=True)
+    fal_bank_state_id = fields.Many2one("res.country.state", 'Bank Fed. State',
+       change_default=True, domain="[('country_id','=',country_id)]")
+    #end here
+
+#end of res_partner_bank()

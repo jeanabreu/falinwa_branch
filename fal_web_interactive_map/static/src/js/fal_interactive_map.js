46eafa117e91159ea84b26fa78780856df6ec0bd
hans.yonathan@falinwa.com <hans.yonathan@falinwa.com>
2015-05-26 16:44:15 +0700

    fixed tool stage button and project followup;

--- fal_web_interactive_map/static/src/js/fal_interactive_map.js
+++ fal_web_interactive_map/static/src/js/fal_interactive_map.js
@@ -3,8 +3,8 @@
   };
   
   function showMarkDescription(mark_id, des_id){
-    var des_top = parseInt(document.getElementById(mark_id).offsetTop, 10) - 129 + 'px';
-    var des_left = parseInt(document.getElementById(mark_id).offsetLeft, 10) - 218 + 'px' ;
+    var des_top = parseInt(document.getElementById(mark_id).offsetTop, 10) - 145 + 'px';
+    var des_left = parseInt(document.getElementById(mark_id).offsetLeft, 10) - 228 + 'px' ;
     document.getElementById(des_id).style.display = "block";
     document.getElementById(des_id).style.top = des_top;
     document.getElementById(des_id).style.left = des_left;

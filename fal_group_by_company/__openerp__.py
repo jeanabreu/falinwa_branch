1239ebb107a2b7f3b3ef5a1a7039205cc3778a5b
hans.yonathan@falinwa.com <hans.yonathan@falinwa.com>
2015-07-03 13:31:01 +0700

    init group by company module;

---
+++ fal_group_by_company/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "GEN-45_Group by Company",
+    "version": "1.0",
+    'author': 'Falinwa Hans',
+    "description": """
+    Module to give group by company
+    
+    """,
+    "depends" : ['sale', 'purchase', 'account', 'account_voucher'],
+    'init_xml': [],
+    'data': [
+    ],
+    'update_xml': [
+        'general_view.xml',
+    ],
+    'css': [],
+    'installable': True,
+    'active': False,
+    'application' : False,
+    'js': [],
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

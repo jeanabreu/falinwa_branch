dd51c7b666fae68cd28323a7a968b467df9c1090
hans.yonathan@falinwa.com <hans.yonathan@falinwa.com>
2015-07-10 11:28:16 +0700

    init account code change;

---
+++ fal_change_account_code/account.py
@@ -0,0 +1,16 @@
+from openerp import fields, models, api
+from openerp.tools.translate import _
+
+class account_account(models.Model):
+    _name = "account.account"
+    _inherit = "account.account"
+    
+    
+    @api.multi
+    def _check_allow_code_change(self):
+        res = True
+        if not res:
+            res = super(account_account, self)._check_allow_code_change()
+        return res
+    
+#end of account_account()

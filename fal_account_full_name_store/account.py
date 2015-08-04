f1f7f81b3d65cbe132aa8bd5eb0030f6f61a4b49
hans.yonathan@falinwa.com <hans.yonathan@falinwa.com>
2015-08-04 13:50:30 +0700

    improve;

--- fal_account_full_name_store/account.py
+++ fal_account_full_name_store/account.py
@@ -5,22 +5,25 @@ class account_account(models.Model):
     _name = "account.account"
     _inherit = "account.account"
 
-    def _get_one_full_name(self, elmt, level=6):
-        if level<=0:
-            return '...'
+    def _get_fal_one_full_name(self, elmt, level=6):
+        #if level<=0:
+        #    return '...'
         if elmt.parent_id:
-            parent_path = self._get_one_full_name(elmt.parent_id, level-1) + " / "
+            parent_path = self._get_fal_one_full_name(elmt.parent_id, level-1) + " / "
         else:
             parent_path = ''
         return parent_path + elmt.code + ' ' + elmt.name
         
-    @api.multi
-    @api.depends('name', 'code', 'parent_id', 'parent_id.code', 'parent_id.name', 'parent_id.type', 'parent_id.active', 'parent_id.fal_complete_name')
-    def _get_full_name(self):
-        for rec in self:
-            rec.fal_complete_name = self._get_one_full_name(rec)
+    @api.one
+    @api.depends('name', 'code', 'parent_id', 'parent_id.code', 'parent_id.name', 'parent_id.type', 'parent_id.active')
+    def _get_fal_full_name(self):
+        children_ids = self.search([('id', 'child_of', self.id)])
+        for children_id in children_ids:
+            print children_id
+            #children_id.write({'fal_complete_name' : self._get_fal_one_full_name(children_id)})
+            children_id.fal_complete_name = self._get_fal_one_full_name(children_id)
         
-    fal_complete_name = fields.Char(compute=_get_full_name,
+    fal_complete_name = fields.Char(compute=_get_fal_full_name,
                            string='Full Name', readonly=True, store=True)
     
 #end of account_account()
@@ -29,12 +32,21 @@ class account_analytic_account(models.Model):
     _name = "account.analytic.account"
     _inherit = "account.analytic.account"
 
+    def _get_fal_one_full_name(self, elmt, level=6):
+        #if level<=0:
+        #    return '...'
+        if elmt.parent_id and not elmt.type == 'template':
+            parent_path = self._get_fal_one_full_name(elmt.parent_id, level-1) + " / "
+        else:
+            parent_path = ''
+        return parent_path + elmt.name
+
     @api.one
     @api.depends('name', 'parent_id', 'state', 'parent_id.name', 'parent_id.state')
-    def _get_full_name(self):
-        self.fal_complete_name = self._get_one_full_name(self)
+    def _get_fal_full_name(self):
+        self.fal_complete_name = self._get_fal_one_full_name(self)
         
-    fal_complete_name = fields.Char(compute=_get_full_name,
+    fal_complete_name = fields.Char(compute=_get_fal_full_name,
                            string='Full Name', readonly=True, store=True)
     
 #end of account_analytic_account()
